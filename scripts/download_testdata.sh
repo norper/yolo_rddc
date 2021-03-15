16e324a032a444dc845f0c9ab3309f4fe8802f9e
norper <53385198+norper@users.noreply.github.com>
2021-03-15 20:13:24 +0800

    Add files via upload

---
+++ scripts/download_testdata.sh
@@ -0,0 +1,8 @@
+echo "downloading test1 dataset..."
+wget https://mycityreport.s3-ap-northeast-1.amazonaws.com/02_RoadDamageDataset/public_data/IEEE_bigdata_RDD2020/test1.tar.gz 2>/dev/null || curl -L https://mycityreport.s3-ap-northeast-1.amazonaws.com/02_RoadDamageDataset/public_data/IEEE_bigdata_RDD2020/test1.tar.gz -O test1.tar.g
+echo "downloading test2 dataset..."
+wget https://mycityreport.s3-ap-northeast-1.amazonaws.com/02_RoadDamageDataset/public_data/IEEE_bigdata_RDD2020/test2.tar.gz 2>/dev/null || curl -L https://mycityreport.s3-ap-northeast-1.amazonaws.com/02_RoadDamageDataset/public_data/IEEE_bigdata_RDD2020/test2.tar.gz -O test2.tar.gz
+tar -xvf test1.tar.gz
+tar -xvf test2.tar.gz
+rm test1.tar.gz test2.tar.gz
+cd -
---
+++ scripts/test_dataset.sh
@@ -0,0 +1,3 @@
+cd yolov5
+bash scripts/download_road2020.sh
+bash scripts/prepare_test.sh
