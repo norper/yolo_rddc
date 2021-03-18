aef3d94e048f255d2b87b3d078caaf9a8a0c2cd8
norper <53385198+norper@users.noreply.github.com>
2021-03-18 09:47:14 +0800

    Update download_road2020_aug.sh

--- scripts/download_road2020_aug.sh
+++ scripts/download_road2020_aug.sh
@@ -1,6 +1,6 @@
 cd datasets/road2020/
 echo "downloading train dataset..."
-gdown https://drive.google.com/file/d/18WdcbCP74hrsua97QN0bwWP6Zx-5dH_U/view?usp=sharing -O train.tar.gz
+gdown https://drive.google.com/uc?id=18WdcbCP74hrsua97QN0bwWP6Zx-5dH_U -O train.tar.gz
 tar -xvf train.tar.gz
 rm train.tar.gz
 cd -
