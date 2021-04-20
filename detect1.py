518f12986f354225a46a99a4f9fceb4d955869cd
norper <53385198+norper@users.noreply.github.com>
2021-04-20 16:03:26 +0800

    Update detect1.py

--- detect1.py
+++ detect1.py
@@ -167,6 +167,7 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--rddc-rs', type=str, default='result.txt', help='set result name')
     parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
